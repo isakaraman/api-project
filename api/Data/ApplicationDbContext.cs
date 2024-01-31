8ace8e9c0259b7292255df0b2827756597b3d69a
İsa Karaman <77787550+isakaraman@users.noreply.github.com>
2024-01-31 23:28:32 +0300

    first commit

---
+++ api/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions dbContextOptions) : base (dbContextOptions)
+        {
+                        
+        }
+
+        public DbSet<Stock> Stock {  get; set; }
+        public DbSet<Comment> Comment { get; set; }
+    }
+}
